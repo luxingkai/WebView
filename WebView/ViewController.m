3fa89a26709e55706fdcda680419a851f89f2ceb
luxingkai <18356285335@163.com>
2020-04-16 16:27:59 +0800

    first commit

--- WebView/ViewController.m
+++ WebView/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface ViewController ()
+@interface ViewController ()<WKNavigationDelegate,WKUIDelegate>
 
 @end
 
@@ -16,8 +17,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
+    configuration.allowsAirPlayForMediaPlayback = NO;
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:[UIScreen mainScreen].bounds configuration:configuration];
+    webView.navigationDelegate = self;
+    webView.UIDelegate = self;
+    [self.view addSubview:webView];
+    
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.w3school.com.cn/js/js_intro.asp"]]];
+    
+    
+    
     // Do any additional setup after loading the view.
 }
 
+#pragma mark --
+
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    
+    NSLog(@"%ld",(long)navigation.effectiveContentMode);
+}
+
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
+    
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    
+}
+
+
+
 
 @end
+
+
